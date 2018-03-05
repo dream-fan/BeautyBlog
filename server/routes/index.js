1e66e0f5a37fb7e9684b019a9254c33e5d84e600
dream fan <1258223261@qq.com>
2018-03-05 10:26:45 +0800

    第一次提交

---
+++ server/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var shortId = require('short-id');
+var path = './public/data/book.json';
+
+router.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+next();
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
